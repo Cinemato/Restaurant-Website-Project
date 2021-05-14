c39949f8defe6579979b18c69d81c255ba0d7b71
Cinemato <alharerekhaled@gmail.com>
2021-05-15 01:22:01 +0300

    Starting PHP Classes

---
+++ Project/Products.php
@@ -0,0 +1,29 @@
+<?php
+class Products{
+    public $db = null;
+
+    public function __construct(Database $db){
+        if(!$db->con) return null;
+
+        $this->db = $db;
+    }
+
+    public function getProducts($table = 'products'){
+        $query = $this->db->con->query("SELECT * FROM {$table}");
+        $rows = array();
+
+        while($row = mysqli_fetch_assoc($query)){
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+
+    public function getProduct($id){
+        $query = $this->db->con->query("SELECT * FROM products WHERE product_id = {$id}");
+        $row = mysqli_fetch_assoc($query);
+        
+        return $row;
+    }
+}
+?>
